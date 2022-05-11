def68c866fe80f558a16c86bb5c7288a58e9b04d
Bruno Galli Miranda <galli.miranda91@gmail.com>
2022-05-11 15:50:38 +0200

    Update init_test_connection.sh
    Adjusted line 19 (echo command) and accidental empty line (24)

--- speedtest/scripts/init_test_connection.sh
+++ speedtest/scripts/init_test_connection.sh
@@ -16,7 +16,7 @@ do
     UPLOAD=$(cat $FILE | grep "Upload:" | awk -F " " '{print $2}')
     PING=$(ping -qc1 google.com 2>&1 | awk -F'/' 'END {print (/^round-trip/? $4:"-100")}')
     PLOSS=$(ping -qc1 google.com | grep "packet loss" | awk -F ',' '{print $3}' | awk '{print $1}')
-    echo "Download: $DOWNLOAD Upload: $UPLOAD Ping: $PING   $TIMESTAMP"
+    echo "Download: $DOWNLOAD Upload: $UPLOAD Ping: $PING  Ploss: $PLOSS  $TIMESTAMP"
     curl -i -XPOST 'http://db:8086/write?db=speedtest' --data-binary "download,host=local value=$DOWNLOAD"
     curl -i -XPOST 'http://db:8086/write?db=speedtest' --data-binary "upload,host=local value=$UPLOAD"
     curl -i -XPOST 'http://db:8086/write?db=speedtest' --data-binary "ping,host=local value=$PING"
