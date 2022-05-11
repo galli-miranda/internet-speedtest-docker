c37465a0e45ea5849726d4b738df095dff033ce4
Bruno Galli Miranda <galli.miranda91@gmail.com>
2022-05-11 13:07:39 +0200

    Include Packet Loss metric - 1st draft
    line 18 prints out a % of packet loss in this ping, however the tool is based on a ping of 1 packet, so the results will either be 0 or 100%. 
    I still find it useful, short spikes can be ignored, while flatlines at 100% mean a complete loss of packages, and a quick back and forth from 0 to 100% means a high packet-loss. 
    Next Steps:  
    - Implement graphana Visualisation (I have no idea how to do that yet) 
    - Implement a rolling average on this data, maybe sth like every 20 pings (preferbly adjustable) - yielding a smoother and more realistic curve. (Consider: from 20 pings, 10 pings had 0% PLOSS, a rolling average would show a packet loss of 50%.)

--- speedtest/scripts/init_test_connection.sh
+++ speedtest/scripts/init_test_connection.sh
@@ -15,10 +15,13 @@ do
     DOWNLOAD=$(cat $FILE | grep "Download:" | awk -F " " '{print $2}')
     UPLOAD=$(cat $FILE | grep "Upload:" | awk -F " " '{print $2}')
     PING=$(ping -qc1 google.com 2>&1 | awk -F'/' 'END {print (/^round-trip/? $4:"-100")}')
+    PLOSS=$(ping -qc1 google.com | grep "packet loss" | awk -F ',' '{print $3}' | awk '{print $1}')
     echo "Download: $DOWNLOAD Upload: $UPLOAD Ping: $PING   $TIMESTAMP"
     curl -i -XPOST 'http://db:8086/write?db=speedtest' --data-binary "download,host=local value=$DOWNLOAD"
     curl -i -XPOST 'http://db:8086/write?db=speedtest' --data-binary "upload,host=local value=$UPLOAD"
     curl -i -XPOST 'http://db:8086/write?db=speedtest' --data-binary "ping,host=local value=$PING"
+    curl -i -XPOST 'http://db:8086/write?db=speedtest' --data-binary "ploss, host=local value=$PLOSS"
+     
     sleep $TEST_INTERVAL
 
 done
